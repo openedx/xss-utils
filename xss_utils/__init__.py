cdbad6b0f8a945a35c7b7d8b6ef6bf54fee39275
Ayub khan <muhammadayubkhan6@gmail.com>
2019-11-06 15:15:08 +0500

    BOM-1010 -updated tox and travis -ran make upgrade. -version and trove classifiers -moved linting & test configs to setup.cfg

--- xss_utils/__init__.py
+++ xss_utils/__init__.py
@@ -4,6 +4,6 @@ Utilities to prevent possible XSS attacks on Django/Mako templates..
 
 from __future__ import absolute_import, unicode_literals
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 default_app_config = 'xss_utils.apps.XssUtilsConfig'  # pylint: disable=invalid-name
