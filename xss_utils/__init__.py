accb3d6ef24b0bb78f4df101cac77ccc0977842f
usamasadiq <usama.sadiq@arbisoft.com>
2021-01-08 15:58:23 +0500

    Update PyPI token

--- xss_utils/__init__.py
+++ xss_utils/__init__.py
@@ -1,6 +1,6 @@
 """
 Utilities to prevent possible XSS attacks on Django/Mako templates..
 """
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 default_app_config = 'xss_utils.apps.XssUtilsConfig'  # pylint: disable=invalid-name
