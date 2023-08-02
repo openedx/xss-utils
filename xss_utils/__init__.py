1646386d283dbb52f7c07a91ca9a4f7fcfd50336
Zubair Shakoor <57657330+zubairshakoorarbisoft@users.noreply.github.com>
2023-08-02 15:46:08 +0500

    fix: Added support for django 42 (#180)

--- xss_utils/__init__.py
+++ xss_utils/__init__.py
@@ -1,6 +1,4 @@
 """
 Utilities to prevent possible XSS attacks on Django/Mako templates..
 """
-__version__ = '0.4.0'
-
-default_app_config = 'xss_utils.apps.XssUtilsConfig'  # pylint: disable=invalid-name
+__version__ = '0.5.0'
